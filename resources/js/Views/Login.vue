58a0a3ec1eec95d443c5d07ed6e45ec834abb361
Motaharul <rimon.raton@gmail.com>
2022-08-06 00:08:53 +0600

    test workflow

--- resources/js/Views/Login.vue
+++ resources/js/Views/Login.vue
@@ -4,10 +4,10 @@
     <div class="flex justify-center">
         <div class="flex-1">
             <div class="border w-auto">
-                <div  class="border p-4  font-semibold">Log In</div>
+                <div  class="border p-4  font-semibold">Log Ins</div>
 
                 <div class="p-4 bg-white">
-                    
+
 
                     <Errors type="danger" v-if="errors" :content="errors" @close="errors=null" />
 
@@ -15,14 +15,14 @@
                         <div class=" w-full my-1 py-2 sm:flex  sm:items-center sm:justify-between">
                             <label for="Email" class="w-4/12 "> Email </label>
                             <input type="email" v-model="email" name="email" class="border border-gray-300 bg-white sm:w-8/12 w-full p-2 mt-3 sm:mt-0 focus:outline-none rounded-sm">
-                            
+
                         </div>
                         <div class=" w-full my-1 py-2 sm:flex  sm:items-center sm:justify-between">
                             <label for="Password" class="w-4/12 "> Password </label>
                             <input type="password" v-model="password" name="password" class="border border-gray-300 bg-white sm:w-8/12 w-full p-2 mt-3 sm:mt-0 focus:outline-none rounded-sm ">
                         </div>
                         <div class=" w-full my-1 py-2 sm:flex  sm:items-center  sm:justify-end">
-                            
+
                             <div class="sm:w-8/12 w-full mt-3 sm:mt-0">
                                 <input type="checkbox" name="Remeber Me" class="mr-2" id="">
                                 <label for="Remember me" >Remeber Me ?</label>
@@ -31,7 +31,7 @@
                         </div>
                         <div class=" w-full my-1 py-2 sm:flex  sm:items-center  sm:justify-end">
                             <div class="sm:w-8/12 w-full  flex justify-between items-center mt-3 sm:mt-0">
-                                <div v-if="busy"  class="flex justify-center items-center p-2 px-6 rounded-sm text-white bg-blue-500 hover:bg-blue-600"> 
+                                <div v-if="busy"  class="flex justify-center items-center p-2 px-6 rounded-sm text-white bg-blue-500 hover:bg-blue-600">
                                    <circle-svg class="w-6 h-6" />
                                 </div>
                                 <button v-else type="submit" class="p-3 rounded-sm text-white bg-blue-500 hover:bg-blue-600">Log In</button>
@@ -43,8 +43,8 @@
                         </div>
                     </form>
 
-                    
-           
+
+
          </div>
             </div>
         </div>
@@ -66,15 +66,15 @@ export default {
     },
     data() {
         return {
-            
-            email : '' , 
-            password : '' , 
+
+            email : '' ,
+            password : '' ,
             errors : null ,
             busy : false ,
 
         }
     },
-    
+
     methods : {
         async login(){
             this.busy = true ;
@@ -87,9 +87,9 @@ export default {
                 this.errors = e.data
             };
             this.busy = false ;
-            
+
         }
     },
-    
+
 }
 </script>
